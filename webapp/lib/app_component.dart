fda23639134dde0718f6545d38ca388f06025d10
KhalidNG <f5f@msn.com>
2017-11-09 00:33:00 -0800

    (add): AngularComponent and SCSS

--- webapp/lib/app_component.dart
+++ webapp/lib/app_component.dart
@@ -1,8 +1,14 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
 
 @Component(
     selector: 'app-component',
-    templateUrl: 'app_component.html')
+    styleUrls: const ['app_component.scss.css'],
+    templateUrl: 'app_component.html',
+    directives: const [materialDirectives],
+     providers: const [materialProviders],
+     )
 class AppComponent {
   var name = 'Angular';
 }
